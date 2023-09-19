4779195849a62e0dbe1d0de0c366b2ce4c50bd48
김예지 <yeji@gim-yejiui-MacBookAir.local>
2023-09-20 00:40:19 +0900

    [Fix] error 수정

--- travel-lion-fe/src/pages/MyPage.jsx
+++ travel-lion-fe/src/pages/MyPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Nav from '../components/MainPage/Nav';
 import MyPageMain from '../components/MyPage/MyPageMain';
-import MyPageHeader from '../components/MyPage/MyPageHeader';
+
 export default function MyPage() {
   return (
     <>

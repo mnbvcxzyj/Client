d5f47c64ce9d3a2afdd714b7928a81ada08cb859
hyunseo <ols3040015@naver.com>
2023-09-21 23:29:11 +0900

    [Fix] conflict 수정 중

--- travel-lion-fe/src/App.js
+++ travel-lion-fe/src/App.js
@@ -1,19 +1,11 @@
 import React from 'react';
 import GlobalStyles from './styles/GlobalStyle';
 import Router from './Router';
-import NormalJoin from './components/Login/NormalJoin';
-import NormalJoin2 from './components/Login/NormalJoin2';
-import Complete from './components/Login/Complete';
-import MyPageHeader from './components/MyPage/MyPageHeader';
-import AccountManage from './components/MyPage/AccountManage';
-import MyPageMain from './components/MyPage/MyPageMain';
-import TravelManage from './components/MyPage/TravelManage';
-
 function App() {
   return (
     <>
       <GlobalStyles />
-      <MyPageMain />
+      <Router />
     </>
   );
 }
--- travel-lion-fe/src/Router.js
+++ travel-lion-fe/src/Router.js
@@ -5,7 +5,8 @@ import MyPage from './pages/MyPage';
 import NewBillPage from './pages/NewBillPage';
 import TravelManage from './components/MyPage/TravelManage';
 import AccountManage from './components/MyPage/AccountManage';
-import Login from './components/Login/Login';
+import BillUpdatePage from './pages/BillUpdatePage';
+import BillListPage from './pages/BIllListPage';
 export default function Router() {
   return (
     <>
@@ -16,7 +17,8 @@ export default function Router() {
           <Route path="/mypage/account" element={<AccountManage />} />
           <Route path="/mypage/travel" element={<TravelManage />} />
           <Route path="/newbill" element={<NewBillPage />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="/billupdate" element={<BillUpdatePage />} />
+          <Route path="/billlist" element={<BillListPage />} />
         </Routes>
       </BrowserRouter>
     </>

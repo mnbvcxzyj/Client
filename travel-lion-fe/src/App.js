67f23a1e2177807c1a9795d264f46b212bc77aaa
hyunseo <ols3040015@naver.com>
2023-09-23 23:06:37 +0900

    [Feat] 프론트 merge

--- travel-lion-fe/src/App.js
+++ travel-lion-fe/src/App.js
@@ -1,19 +1,11 @@
 import React from 'react';
 import GlobalStyles from './styles/GlobalStyle';
 import Router from './Router';
-import AccountManage from './components/MyPage/AccountManage';
-import MyPageHeader from './components/MyPage/MyPageHeader';
-import MyPageMain from './components/MyPage/MyPageMain';
-import OldPasswd from './components/MyPage/OldPasswd';
-import ChangePasswd from './components/MyPage/ChangePasswd';
-import ChangeName from './components/MyPage/ChangeName';
-import Modal from './components/MyPage/Modal';
-
 function App() {
   return (
     <>
       <GlobalStyles />
-      <AccountManage />
+      <Router />
     </>
   );
 }

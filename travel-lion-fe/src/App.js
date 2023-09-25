4c8e62069d6b5f619d6f18ed334ab78f91a25285
hyunseo <ols3040015@naver.com>
2023-09-25 17:02:20 +0900

    [Feat] 배포

--- travel-lion-fe/src/App.js
+++ travel-lion-fe/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import GlobalStyles from './styles/GlobalStyle';
 import Router from './Router';
+import NoAccount from './components/MyPage/NoAccount';
+
 function App() {
   return (
     <>
       <GlobalStyles />
-      <Router />
+      <NoAccount />
     </>
   );
 }

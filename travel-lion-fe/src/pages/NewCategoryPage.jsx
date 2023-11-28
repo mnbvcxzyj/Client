be1e6dac0ba108470787fe729f30601218b57921
YunKyeong <alzkdpffk4314@gmail.com>
2023-11-28 23:56:38 +0900

    [Feat]카테고리추가(미완성), [Fix] 작성, 수정 디자인 개선

---
+++ travel-lion-fe/src/pages/NewCategoryPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import NewCategory from '../components/NewCategory/NewCategory';
+
+const NewCategoryPage = () => {
+  return (
+    <div>
+      <NewCategory />
+    </div>
+  );
+};
+
+export default NewCategoryPage;
